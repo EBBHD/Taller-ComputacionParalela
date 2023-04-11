c8a9e164639a60169d3c6a7c5d723bc73b06e0d6
MHT <75126964+EBBHD@users.noreply.github.com>
2023-04-10 23:38:37 -0400

    Update Main

--- main_1.cpp
+++ main_1.cpp
@@ -22,23 +22,23 @@ int main() {
     if (discriminante > 0) {                                                                                //Calculo de la solución cuando el discriminante es mayor a 0
         double raiz1 = (-coeficientes[grado - 1] + sqrt(discriminante)) / (2 * coeficientes[grado]);
         double raiz2 = (-coeficientes[grado - 1] - sqrt(discriminante)) / (2 * coeficientes[grado]);
-        cout <<"--Integrantes--";
-        cout <<"Javier Uribe Valenzuela";
-        cout <<"Martín Hurtado";
+        cout <<"\n--Integrantes--";
+        cout <<"\nJavier Uribe Valenzuela";
+        cout <<"\nMartín Hurtado";
         cout << "Las raíces son: " << raiz1 << " y " << raiz2 << endl;
     } else if (discriminante == 0) {                                                                        //Calculo de las soluciones cuando el discriminante es igual a 0
         double raiz = -coeficientes[grado - 1] / (2 * coeficientes[grado]);
-        cout <<"--Integrantes--";
-        cout <<"Javier Uribe Valenzuela";
-        cout <<"Martín Hurtado";
-        cout << "La única raíz es: " << raiz << endl;
+        cout <<"\n--Integrantes--";
+        cout <<"\nJavier Uribe Valenzuela";
+        cout <<"\nMartín Hurtado";
+        cout << "\nLa única raíz es: " << raiz << endl;
     } else {                                                                                                //Calculo de las soluciones cuando el discriminante es menor a 0
         double parteReal = -coeficientes[grado - 1] / (2 * coeficientes[grado]);
         double parteImaginaria = sqrt(-discriminante) / (2 * coeficientes[grado]);
-        cout << "Las raíces son complejas: " << parteReal << " + " << parteImaginaria << "i y " << parteReal << " - " << parteImaginaria << "i" << endl;
-        cout <<"--Integrantes--";
-        cout <<"Javier Uribe Valenzuela";
-        cout <<"Martín Hurtado";
+        cout << "\nLas raíces son complejas: " << parteReal << " + " << parteImaginaria << "i y " << parteReal << " - " << parteImaginaria << "i" << endl;
+        cout <<"\n--Integrantes--";
+        cout <<"\nJavier Uribe Valenzuela";
+        cout <<"\nMartín Hurtado";
     }
 
     return 0;
